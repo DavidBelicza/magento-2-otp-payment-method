e3faa85c285f0ed29d4e3ad2e5d66cf6fe8b2438
Dominik Nsosso <dominik.nsosso@immediate.co.uk>
2018-10-07 15:21:26 +0100

    Add Create invoice if the order is Virtual. Add Order status to complete if the order is virtual.

--- Model/Order/StatusUpdater.php
+++ Model/Order/StatusUpdater.php
@@ -12,8 +12,10 @@ namespace Youama\OTP\Model\Order;
 
 use Magento\Sales\Api\Data\OrderStatusHistoryInterfaceFactory;
 use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Api\InvoiceRepositoryInterface;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Email\Sender\OrderCommentSender;
+use Magento\Sales\Model\Order\Email\Sender\InvoiceSender;
 use Magento\Sales\Model\Order\Status\History;
 use Youama\OTP\Api\OrderFinderInterface;
 use Youama\OTP\Helper\Config;
@@ -57,28 +59,50 @@ class StatusUpdater
      * @var Config
      */
     private $configHelper;
+    /**
+     * @var \Magento\Sales\Model\Service\InvoiceService
+     */
+    protected $invoiceService;
 
     /**
-     * Status constructor.
-     *
-     * @param OrderRepositoryInterface           $orderRepository
-     * @param OrderFinderInterface               $orderFinder
+     * @var \Magento\Framework\DB\Transaction
+     */
+    protected $transaction;
+
+    protected $invoiceSender;
+
+    protected $invoiceRepository;
+
+    /**
+     * StatusUpdater constructor.
+     * @param OrderRepositoryInterface $orderRepository
+     * @param OrderFinderInterface $orderFinder
      * @param OrderStatusHistoryInterfaceFactory $orderStatusHistoryInterfaceFactory
-     * @param OrderCommentSender                 $orderCommentSender
-     * @param Config                             $configHelper
+     * @param OrderCommentSender $orderCommentSender
+     * @param Config $configHelper
+     * @param \Magento\Sales\Model\Service\InvoiceService $invoiceService
+     * @param \Magento\Framework\DB\Transaction $transaction
      */
     public function __construct(
         OrderRepositoryInterface $orderRepository,
         OrderFinderInterface $orderFinder,
         OrderStatusHistoryInterfaceFactory $orderStatusHistoryInterfaceFactory,
         OrderCommentSender $orderCommentSender,
-        Config $configHelper
+        Config $configHelper,
+        \Magento\Sales\Model\Service\InvoiceService $invoiceService,
+        \Magento\Framework\DB\Transaction $transaction,
+        InvoiceSender $invoiceSender,
+        InvoiceRepositoryInterface $invoiceRepository
     ) {
         $this->orderRepository = $orderRepository;
         $this->orderFinder = $orderFinder;
         $this->orderStatusHistoryInterfaceFactory = $orderStatusHistoryInterfaceFactory;
         $this->orderCommentSender = $orderCommentSender;
         $this->configHelper = $configHelper;
+        $this->invoiceService = $invoiceService;
+        $this->transaction = $transaction;
+        $this->invoiceSender = $invoiceSender;
+        $this->invoiceRepository = $invoiceRepository;
     }
 
     /**
@@ -97,15 +121,46 @@ class StatusUpdater
     }
 
     /**
+     * @param Order $order
+     */
+    private function createInvoice(Order $order)
+    {
+        if ($order->canInvoice()) {
+            $invoice = $this->invoiceService->prepareInvoice($order);
+            $invoice->register();
+            $invoice->setState(Order\Invoice::STATE_PAID);
+            $this->invoiceRepository->save($invoice);
+            $transactionSave = $this->transaction->addObject(
+                $invoice
+            )->addObject(
+                $invoice->getOrder()
+            );
+            $transactionSave->save();
+            $this->invoiceSender->send($invoice);
+            //send notification code
+            /** @var OrderRepositoryInterface|History $history */
+            $history = $this->orderStatusHistoryInterfaceFactory->create();
+
+            $history->setComment(__('Notified customer about invoice #%1.', $invoice->getId()));
+            $history->setIsVisibleOnFront(0);
+            $history->setIsCustomerNotified(1);
+            $history->setStatus(Order::STATE_COMPLETE);
+
+            $order->addStatusHistory($history);
+            $order->setStatus(Order::STATE_COMPLETE);
+            $this->orderRepository->save($order);
+        }
+    }
+
+    /**
      * Put order to processing, notify customer about it.
      *
      * @param string $orderIncrementId
-     * @param int    $transactionId
+     * @param int $transactionId
      */
     public function success(string $orderIncrementId, int $transactionId)
     {
         $order = $this->orderFinder->getOrderByOrderIncrementId($orderIncrementId);
-
         if ($order->getStatus() == Order::STATE_PENDING_PAYMENT) {
             $comment = str_replace(
                 self::PLACEHOLDER,
@@ -126,6 +181,12 @@ class StatusUpdater
 
             $this->orderRepository->save($order);
             $this->orderCommentSender->send($order, true, $comment);
+
+
+            /** Create Invoice if order was virtual */
+            if ($order->getIsVirtual()) {
+                $this->createInvoice($order);
+            }
         }
     }
 
